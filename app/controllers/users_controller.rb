e7a494c4ce0314aa3c8276cea384d4211fa2d4e0
rathersilly <rathersilly@live.com>
2021-02-06 01:54:17 -0600

    13.2 done - Showing microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,34 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-before_action :correct_user,   only: [:edit, :update]
-before_action :admin_user,     only: [:destroy]
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user,   only: %i[edit update]
+  before_action :admin_user,     only: [:destroy]
 
-def index
-  #@users = User.all
-  @users = User.where(activated: true).paginate(page: params[:page])
-end
+  def index
+    # @users = User.all
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
 
-def new
+  def new
     @user = User.new
-    #debugger
+    # debugger
   end
 
   def create
-    @user = User.new(user_params) #not the final implementation!
-    
+    @user = User.new(user_params) # not the final implementation!
+
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
-      #log_in @user
+      # log_in @user
       # handle a successful save.
-      #flash[:success] = "Welcome to the Sample App!"
-      
-      #redirect_to @user
+      # flash[:success] = "Welcome to the Sample App!"
+
+      # redirect_to @user
       # same as redirect_to user_url(@user)
     else
       render 'new'
-    end    
+    end
   end
 
   def edit
@@ -38,50 +38,50 @@ def new
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = 'Profile updated'
       redirect_to @user
     else
       render 'edit'
     end
   end
 
-
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated?
-    #debugger
+    # debugger
   end
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-    # Before filters
+  # Before filters
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
     end
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url)  unless current_user?(@user)
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
-    # Confirm an admin user.
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  # Confirm an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name:                  "Example User",
-            email:                  "example@railstutorial.org",
+            email:                  "example@example.com",
             password:               "foobar",
             password_confirmation:  "foobar",
             admin: true,
@@ -15,7 +15,7 @@ User.create!(name:                  "Example User",
             activated_at: Time.zone.now)
 
 
-99.times do |n|
+49.times do |n|
     name = Faker::Name.name
     email = "example-#{n+1}@railstutorial.org"
     password = "password"
@@ -25,4 +25,10 @@ User.create!(name:                  "Example User",
                 password_confirmation:  password,
                 activated: true,
                 activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do 
+    content = Faker::Lorem.sentence(5)
+    users.each { |user| user.microposts.create!(content: content) }
 end
