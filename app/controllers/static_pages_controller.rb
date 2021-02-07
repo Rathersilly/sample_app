8177e476d4e28375f04c2044969dfe811c65d5a5
rathersilly <rathersilly@live.com>
2021-02-07 01:10:20 -0600

    13.3.2 done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,16 +2,12 @@ class StaticPagesController < ApplicationController
   def home
     # flash[:wtf] = 'WTF ' * 10
     # flash[:success] = "HIHIHHI"
-    
+    @micropost = current_user.microposts.build if logged_in?
   end
 
-  def help
-  end
-  
-  def about
-  end
+  def help; end
 
-  def contact
-  end
+  def about; end
 
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 
   #get 'users/new'
   # see Rails Tutorial 5.3.2 to see how root_path etc are formed
